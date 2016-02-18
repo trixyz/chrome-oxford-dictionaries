c9858be7cf5598620f9a8bf68728f81303583bed
trixyz <trixyz.pp@gmail.com>
2016-02-18 15:46:36 +0300

    nearly done

--- background.js
+++ background.js
@@ -51,10 +51,8 @@ chrome.runtime.onMessage.addListener(
     //send a message that contains parsed data to content script
     getWord(request.word, lang.ENG, function(data){
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-              chrome.tabs.sendMessage(tabs[0].id, {defs: data}, function(response) {
-                console.log(response.farewell);
-              });
-            });     
+            chrome.tabs.sendMessage(tabs[0].id, {defs: data}, function(response) {});
+        });     
     });
 });
 
--- inject.js
+++ inject.js
@@ -1,37 +1,126 @@
 "use strict";
 
-var x,y;
+var popup = new Object();
+popup.id = "oxPopup";
+popup.style =  {
+    position:  "absolute",
+    backgroundColor: "white",
+    border: "3px double black",
+    width: "500px",
+    height: "300px",
+    top: "0px",
+    left: "0px",
+    padding: "5px",
+    zIndex: 20,
+    overflow: "auto"
+}
+popup.loading = chrome.extension.getURL("loading.gif");
+popup.hasDrown = false;
 
-function makePopup(data){
-    var text = '';
+
+function createPopup(data){
+    var popupBody = document.createElement("div");
+
+    popupBody.id = data.id;
+    popupBody.style.position = data.style["position"];
+    popupBody.style.backgroundColor = data.style["backgroundColor"];
+    popupBody.style.border = data.style["border"];
+    popupBody.style.width = data.style["width"];
+    popupBody.style.height = data.style["height"];
+    popupBody.style.top = data.style["top"];
+    popupBody.style.left = data.style["left"];
+    popupBody.style.padding = data.style["padding"];
+    popupBody.style.zIndex = data.style["zIndex"];
+    popupBody.style.overflow = data.style["overflow"];
+
+    var loading = document.createElement("img");
+
+    loading.style.display = "block";
+    loading.style.marginLeft = "auto";
+    loading.style.marginRight = "auto";
+    loading.id = "imgLoading";
+    loading.src = data.loading;
+    popupBody.appendChild(loading);
+
+    return popupBody;
+}
+
+function addData(data){
     data = JSON.parse(data);
-    text += '<b>'+data.word+'</b><br>';
+    var popupChild = document.createElement('div');
+    popupChild.id = 'oxChild';
+    var word = document.createElement('h1');
+    word.innerHTML = data.word;
+
+    popupChild.appendChild(word);
     for (var sense of data.senses){
-        text+='As ';
         for (var def of sense){
-            text+=def+'<br>';
+            var elem = document.createElement('p');
+            elem.innerHTML = def;
+            popupChild.appendChild(elem);
         }
     }
-    return text;
+
+    document.getElementById('oxPopup').removeChild(document.getElementById("imgLoading"));
+    document.getElementById('oxPopup').appendChild(popupChild);
 
 }
 
-document.addEventListener('dblclick', function (event) {
 
+function removePopup(){
+    var pop = document.getElementById('oxPopup'); 
+    var dad = pop.parentNode;
+    dad.removeChild(pop);
+    popup.innerHTML = "";
+    popup.hasDrown = false;
+}
+
+document.onclick = function(e) {
+    console.log(e.target);
+    console.log(e.target.id);
+    if (e.target.id == "oxChild"){
+        console.log('epta');
+    }
+    if(popup.hasDrown && e.target != document.getElementById('oxPopup') && e.target.id != "oxChild") {
+        removePopup();
+    }
+}
+
+document.addEventListener('dblclick', function (event) {
+    console.log(popup.hasDrown);
+    if (popup.hasDrown){
+        removePopup();
+        return;
+    }
     var text;
-    x = event.clientX;
-    y = event.clientY;
-    text = window.getSelection();
-    var raw = text.toString();
-    chrome.runtime.sendMessage({word: raw}, function(response) {});
+    var x = event.pageX;
+    console.log('x is '+ event.pageX);
+    if (event.clientX+500>window.innerWidth){
+        x -= 500;
+    }
+    var y = event.pageY;
+    console.log('y is '+ event.pageY);
+    if (event.clientY+300>window.innerHeight){
+        y -= 300;
+    }
+    if (window.getSelection){
+        text = window.getSelection().toString();
+        popup.style["top"] = String(y+10)+"px";
+        popup.style["left"] = String(x+10)+"px";
+        document.body.appendChild(createPopup(popup));
+        popup.hasDrown = true;
+        chrome.runtime.sendMessage({word: text}, function(response) {});
+    } else {console.log('no selection')}
+    
 
 });
+
+
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         var data = request.defs
-        document.body.innerHTML += makePopup(data);
-        
+        addData(data);
   });
 
 
